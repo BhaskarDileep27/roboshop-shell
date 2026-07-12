e19a4e8b7a405ca7d4ebfbd9814e72796e0a2a3e
BhaskarDileep27 <bhaskardileep27@gmail.com>
2026-07-12 08:30:05 +0530

    robashop-shell

---
+++ catalogue.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "ERROR:: $2 ... $R FAILED $N"
+        EXIT 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+}
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current NodeJS"
