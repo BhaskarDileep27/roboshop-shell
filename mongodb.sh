518e3ec4bcf77f74df8508014320f47b635b3e7b
BhaskarDileep27 <bhaskardileep27@gmail.com>
2026-07-12 08:03:33 +0530

    roboshop-shell

--- mongodb.sh
+++ mongodb.sh
@@ -31,3 +31,8 @@ fi # fi means reverse of if, indicating condition end
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo"
+
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing MongoDB"
+
