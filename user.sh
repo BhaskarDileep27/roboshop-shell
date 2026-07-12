97b86e82915200e4d30fb14258edea3c69fcc228
BhaskarDileep27 <bhaskardileep27@gmail.com>
2026-07-12 22:54:16 +0530

    robo-shall cart

--- user.sh
+++ user.sh
@@ -93,7 +93,7 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "Installing Mongodb client"
 
-mongosh --host $MONGDB_HOST </app/schema/user.js &>> $LOGFILE
+mongo --host $MONGDB_HOST </app/schema/user.js &>> $LOGFILE
 
 VALIDATE $? "Loading user data into Mongosh"
 
