da2b572c4866a6307e338a356f6ac359888e9f15
BhaskarDileep27 <bhaskardileep27@gmail.com>
2026-07-27 22:07:32 +0530

    update roboshop.sh

--- web.sh
+++ web.sh
@@ -5,12 +5,12 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MONGDB_HOST=mongodb.dileep.sbs
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -25,43 +25,34 @@ VALIDATE(){
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1 # you can give other than 0
+    exit 1
 else
     echo "You are root user"
-fi # fi means reverse of if, indicating condition end
+fi
 
 dnf install nginx -y &>> $LOGFILE
- 
 VALIDATE $? "Installing nginx"
 
 systemctl enable nginx &>> $LOGFILE
-
 VALIDATE $? "Enable nginx" 
 
 systemctl start nginx &>> $LOGFILE
-
 VALIDATE $? "Starting Nginx"
 
 rm -rf /usr/share/nginx/html/* &>> $LOGFILE
-
-VALIDATE $? "removed default website"
+VALIDATE $? "Removed default website"
 
 curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
-
 VALIDATE $? "Downloaded web application"
 
 cd /usr/share/nginx/html &>> $LOGFILE
-
-VALIDATE $? "moving nginx html directory"
+VALIDATE $? "Moving to nginx html directory"
 
 unzip -o /tmp/web.zip &>> $LOGFILE
-
-VALIDATE $? "unzipping web"
+VALIDATE $? "Unzipping web"
  
-cp /home/ec2-user/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE 
-
-VALIDATE $? "copied roboshop reverse proxy config"
+cp $SCRIPT_DIR/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE 
+VALIDATE $? "Copied roboshop reverse proxy config"
 
 systemctl restart nginx &>> $LOGFILE
-
-VALIDATE $? "restarted nginx"
+VALIDATE $? "Restarted nginx"
