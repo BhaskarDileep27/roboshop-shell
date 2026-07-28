801182979f6a0862ba219f7642f034c99b12a4af
BhaskarDileep27 <bhaskardileep27@gmail.com>
2026-07-28 20:43:43 +0530

    update roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ set -e
 AMI="ami-0220d79f3f480ecf5" # this keeps on changing
 SG_ID="sg-0e75a9a19ca1ccce6" # replace with your SG ID
 AWS_REGION="${AWS_REGION:-us-east-1}"
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "web" "payment" "dispatch" "shipping")
 ZONE_ID="Z10151982EVZUHG3VSVN6" # replace your zone ID
 DOMAIN_NAME="dileep.sbs"
 
